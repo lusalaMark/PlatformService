5238ad113598481b6b528612cd7ef9744934b8c1
lusalaMark <lusalamark63@gmail.com>
2022-03-19 22:21:49 +0300

    registered the service for the model

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.models;
+
+namespace PlatformService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+
+    }
+}
